d56668e954de4d479d73975e30f626b5397abb84
Le Tu <lethucthanhtu@gmail.com>
2023-06-09 14:04:54 +0700

    add controller of account, application

--- src/main/java/com/findhub/finhubbackend/controller/AccountController.java
+++ src/main/java/com/findhub/finhubbackend/controller/AccountController.java
@@ -3,30 +3,47 @@ package com.findhub.finhubbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.findhub.finhubbackend.entity.account.Account;
+import com.findhub.finhubbackend.entity.account.AccountStatus;
 import com.findhub.finhubbackend.service.account.AccountService;
+import com.findhub.finhubbackend.util.Config.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping(path = "/api/accounts")
+@RequestMapping(path = Path.ACCOUNT)
 public class AccountController {
 
 	@Autowired
 	private AccountService accountService;
 
-	@GetMapping("/getAll")
+	@GetMapping(Path.ALL)
 	public List<Account> getAccounts() {
 		return accountService.getAll();
 	}
 
-	@GetMapping("/{id}")
-	public Account getAccountById(@PathVariable("id") int id) {
+	@GetMapping(Path.ID)
+	public Account getAccountById(@PathVariable(Var.ID) int id) {
 		return accountService.findById(id);
+	}
+
+	public Account addAccount(Account account) {
+		return accountService.add(account);
+	}
+
+	@PostMapping(Path.UPDATE)
+	public Account updateAccount(@PathVariable(Var.ID) int id, @RequestBody Account account) {
+		return accountService.update(id, account);
+	}
+
+	@PostMapping(Path.DELETE)
+	public boolean deleteAccount(@RequestBody int id) {
+		return accountService.changeStatus(id, AccountStatus.INACTIVE);
+	}
+
+	@PostMapping(Path.RESTORE)
+	public boolean restoreAccount(@RequestBody int id) {
+		return accountService.changeStatus(id, AccountStatus.ACTIVE);
 	}
 }
--- src/main/java/com/findhub/finhubbackend/controller/ApplicationController.java
+++ src/main/java/com/findhub/finhubbackend/controller/ApplicationController.java
@@ -1,17 +1,59 @@
 package com.findhub.finhubbackend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.findhub.finhubbackend.entity.application.Application;
+import com.findhub.finhubbackend.entity.application.ApplicationStatus;
 import com.findhub.finhubbackend.service.application.ApplicationService;
+import com.findhub.finhubbackend.util.Config.*;
 
 @RestController
 @CrossOrigin
-@RequestMapping(path = "/api/applications")
+@RequestMapping(path = Path.APPLICATION)
 public class ApplicationController {
-	
+
 	@Autowired
 	private ApplicationService applicationService;
+
+	@GetMapping(Path.ALL)
+	public List<Application> getAccounts() {
+		return applicationService.getAll();
+	}
+
+	@GetMapping(Path.ID)
+	public Application getAccountById(@PathVariable("id") int id) {
+		return applicationService.findById(id);
+	}
+
+	public Application addApplication(Application application) {
+		return applicationService.add(application);
+	}
+
+	@PostMapping(Path.UPDATE)
+	public Application updateApplication(@PathVariable(Var.ID) int id, @RequestBody Application application) {
+		return applicationService.update(id, application);
+	}
+
+	@PostMapping(Path.DELETE)
+	public boolean deleteApplication(@RequestBody int id) {
+		return applicationService.changeStatus(id, ApplicationStatus.DELETED);
+	}
+
+	@PostMapping(Path.RESTORE)
+	public boolean restoreApplication(@RequestBody int id) {
+		return applicationService.changeStatus(id, ApplicationStatus.PENDING);
+	}
+
+	public boolean changeStatusApplication(@RequestBody int id, @RequestBody int status) {
+		return applicationService.changeStatus(id, status);
+	}
 }
