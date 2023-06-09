f0699f874e0cc04e9d94ca75d8e110311e7dbe30
Le Tu <lethucthanhtu@gmail.com>
2023-06-09 10:10:15 +0700

    fix account api document

--- src/main/java/com/findhub/finhubbackend/service/account/AccountService.java
+++ src/main/java/com/findhub/finhubbackend/service/account/AccountService.java
@@ -5,34 +5,78 @@ import java.util.List;
 import com.findhub.finhubbackend.entity.account.Account;
 
 public interface AccountService {
-
+	/**
+	 * add new Account to DB
+	 */
 	public Account add(Account account);
 
+	/**
+	 * update Account
+	 */
 	public Account update(int id, Account account);
 
+	/**
+	 * update Account
+	 */
 	public Account update(Account oldAccount, Account newAccount);
 
+	/**
+	 * set new status of Account
+	 */
 	public boolean changeStatus(int id, int status);
 
+	/**
+	 * set new status of Account
+	 */
 	public boolean changeStatus(Account account, int status);
 
+	/**
+	 * delete existed Account from DB
+	 */
 	public boolean delete(int id);
 
+	/**
+	 * delete existed Account from DB
+	 */
 	public boolean delete(Account account);
 
+	/**
+	 * find account by id (exact id)
+	 */
 	public Account findById(int id);
 
+	/**
+	 * find account by email (exact Email)
+	 */
 	public Account findByEmail(String email);
 
+	/**
+	 * get all accounts in DB
+	 */
 	public List<Account> getAll();
 
+	/**
+	 * find accounts by id (approximate id)
+	 */
 	public List<Account> findByIdLike(int id);
 
+	/**
+	 * find accounts by email (approximate Email)
+	 */
 	public List<Account> findByEmailLike(String email);
 
+	/**
+	 * find accounts by status
+	 */
 	public List<Account> findByStatus(int status);
 
+	/**
+	 * find accounts by role
+	 */
 	public List<Account> findByRole(int role);
 
+	/**
+	 * save account
+	 */
 	public void save(Account account);
 }
--- src/main/java/com/findhub/finhubbackend/service/account/AccountServiceImpl.java
+++ src/main/java/com/findhub/finhubbackend/service/account/AccountServiceImpl.java
@@ -16,17 +16,11 @@ public class AccountServiceImpl implements AccountService {
 	@Autowired
 	private AccountRepository accountRepository;
 
-	/**
-	 * add new Account to DB
-	 */
 	@Override
 	public Account add(Account account) {
 		return (account != null) ? accountRepository.save(account) : null;
 	}
 
-	/**
-	 * set new status of Account
-	 */
 	@Override
 	public boolean changeStatus(Account account, int status) {
 		if (account != null) {
@@ -36,18 +30,12 @@ public class AccountServiceImpl implements AccountService {
 		return false;
 	}
 
-	/**
-	 * set new status of Account
-	 */
 	@Override
 	public boolean changeStatus(int id, int status) {
 		Optional<Account> account = accountRepository.findById(id);
 		return changeStatus(account.get(), status);
 	}
 
-	/**
-	 * delete existed Account from DB
-	 */
 	@Override
 	public boolean delete(Account account) {
 		if (account != null) {
@@ -57,18 +45,12 @@ public class AccountServiceImpl implements AccountService {
 		return false;
 	}
 
-	/**
-	 * delete existed Account from DB
-	 */
 	@Override
 	public boolean delete(int id) {
 		Optional<Account> account = accountRepository.findById(id);
 		return delete(account.get());
 	}
 
-	/**
-	 * update Account
-	 */
 	@Override
 	public Account update(Account oldAccount, Account newAccount) {
 		if (newAccount != null && oldAccount != null) {
@@ -80,76 +62,49 @@ public class AccountServiceImpl implements AccountService {
 		return null;
 	}
 
-	/**
-	 * update Account
-	 */
 	@Override
 	public Account update(int id, Account account) {
 		Optional<Account> old = accountRepository.findById(id);
 		return update(old.isPresent() ? old.get() : null, account);
 	}
 
-	/**
-	 * get all accounts in DB
-	 */
 	@Override
 	public List<Account> getAll() {
 		return accountRepository.findAll();
 	}
 
-	/**
-	 * find account by id (exact id)
-	 */
 	@Override
 	public Account findById(int id) {
 		Optional<Account> account = accountRepository.findById(id);
 		return account.isPresent() ? account.get() : null;
 	}
 
-	/**
-	 * find account by email (exact Email)
-	 */
 	@Override
 	public Account findByEmail(String email) {
 		Optional<Account> account = accountRepository.findByEmail(email);
 		return account.isPresent() ? account.get() : null;
 	}
 
-	/**
-	 * find accounts by id (approximate id)
-	 */
 	@Override
 	public List<Account> findByIdLike(int id) {
 		return accountRepository.findByIdLike(id);
 	}
 
-	/**
-	 * find accounts by email (approximate Email)
-	 */
 	@Override
 	public List<Account> findByEmailLike(String email) {
 		return accountRepository.findByEmailLike(email);
 	}
 
-	/**
-	 * find accounts by role
-	 */
 	@Override
 	public List<Account> findByRole(int role) {
 		return accountRepository.findByRole(role);
 	}
 
-	/**
-	 * find accounts by status
-	 */
 	@Override
 	public List<Account> findByStatus(int status) {
 		return accountRepository.findByStatus(status);
 	}
 
-	/**
-	 * save account
-	 */
 	@Override
 	public void save(Account account) {
 		accountRepository.save(account);
